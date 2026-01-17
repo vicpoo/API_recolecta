2fc5a1afeb0e388c53c10b126f1083a41a6bd2af
Luis Fernando <105691753+Luis1f2@users.noreply.github.com>
2026-01-17 13:29:09 -0600

    Delete src/notificacion directory

--- src/notificacion/domain/repository.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type NotificacionRepository interface {
-	Create(n *Notificacion) error
-	GetByID(id int) (*Notificacion, error)
-	GetByUsuario(usuarioID int) ([]Notificacion, error)
-	Deactivate(id int) error
-}
