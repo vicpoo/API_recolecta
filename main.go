0d04addc4280b85a7262be4df05c770e9a629433
regiber montoya gonzalez <233387@ids.upchiapas.edu.mx>
2026-05-03 18:35:22 -0600

    feat(seeder): agregar admin seed y opción de seed en main

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
+	"github.com/vicpoo/API_recolecta/src/bootstrap"
+)
 
 func main() {
+	_ = godotenv.Load()
+
+	if len(os.Args) > 1 {
+		switch strings.ToLower(strings.TrimSpace(os.Args[1])) {
+		case "seed", "bootstrap":
+			if err := bootstrap.SeedAdmin(context.Background()); err != nil {
+				fmt.Fprintf(os.Stderr, "bootstrap failed: %v\n", err)
+				os.Exit(1)
+			}
+			return
+		}
+	}
+
 	InitDependencies()
 }
